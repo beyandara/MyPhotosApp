71808b80e82a5a52210c8cc4743fdfaf8d5fbe4a
Therese IV <theresebvik@gmail.com>
2024-04-26 14:00:04 +0200

    fixed tiny error in application

--- app/src/main/java/com/example/albumapp/AlbumApplication.kt
+++ app/src/main/java/com/example/albumapp/AlbumApplication.kt
@@ -9,6 +9,6 @@ class AlbumApplication : Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container = DefaultAppContainer()
+        container = DefaultAppContainer(this)
     }
 }
