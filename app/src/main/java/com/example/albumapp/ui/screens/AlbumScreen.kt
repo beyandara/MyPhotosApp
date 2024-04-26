bf738153b0ef6394e7826faa0e89aa5a94284ef8
beyandara <beyan97@hotmail.com>
2024-04-26 12:29:44 +0200

    added divided screen for albumscreen

--- app/src/main/java/com/example/albumapp/ui/screens/AlbumScreen.kt
+++ app/src/main/java/com/example/albumapp/ui/screens/AlbumScreen.kt
@@ -15,11 +15,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -64,25 +66,38 @@ fun AlbumScreenLayout(
     contentPadding: PaddingValues,
     modifier: Modifier
 ) {
-    Column {
-        PhotosGridScreen(
-            photos,
-            onShowButtonClicked = onShowButtonClicked,
-            onSaveOrDeleteButtonClicked = onDeleteButtonClicked,
-            contentPadding = contentPadding,
-            modifier = modifier.fillMaxWidth()
-        )
-        PhotosGridScreen(
-            photos,
-            onShowButtonClicked = onShowButtonClicked,
-            onSaveOrDeleteButtonClicked = onSaveButtonClicked,
-            contentPadding = contentPadding,
-            modifier = modifier.fillMaxWidth()
-        )
-
-
+    Column() {
+        Column(
+            modifier = Modifier.weight(1.5f)
+        ) {
+            PhotosGridScreen(
+                photos,
+                delete = true,
+                onShowButtonClicked = onShowButtonClicked,
+                onSaveOrDeleteButtonClicked = onDeleteButtonClicked,
+                contentPadding = contentPadding,
+                modifier = modifier
+                    .fillMaxWidth()
+            )
+        }
+        Divider(color = Color.Black, thickness = 3.dp)
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            PhotosGridScreen(
+                photos,
+                delete = false,
+                onShowButtonClicked = onShowButtonClicked,
+                onSaveOrDeleteButtonClicked = onSaveButtonClicked,
+                contentPadding = contentPadding,
+                modifier = modifier
+                    .fillMaxWidth()
+            )
+        }
     }
 }
+
+
 
 /**
  * The home screen displaying the loading message.
@@ -122,6 +137,7 @@ fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
 @Composable
 fun PhotosGridScreen(
     photos: List<Photo>,
+    delete: Boolean,
     onShowButtonClicked: (Photo) -> Unit,
     onSaveOrDeleteButtonClicked: (Photo) -> Unit,
     modifier: Modifier = Modifier,
@@ -139,10 +155,11 @@ fun PhotosGridScreen(
         ) { photo ->
             MarsPhotoCard(
                 photo = photo,
+                delete = delete,
                 onShowButtonClicked = onShowButtonClicked,
                 onSaveOrDeleteButtonClicked = onSaveOrDeleteButtonClicked,
                 modifier = modifier
-//                    .padding(4.dp)
+                    .padding(4.dp)
                     .fillMaxWidth()
 //                    .aspectRatio(1.5f)
             )
@@ -153,6 +170,7 @@ fun PhotosGridScreen(
 @Composable
 fun MarsPhotoCard(
     photo: Photo,
+    delete: Boolean,
     onShowButtonClicked: (Photo) -> Unit,
     onSaveOrDeleteButtonClicked: (Photo) -> Unit,
     modifier: Modifier = Modifier) {
@@ -171,24 +189,23 @@ fun MarsPhotoCard(
                 placeholder = painterResource(R.drawable.loading_img),
                 contentDescription = "",
                 contentScale = ContentScale.FillWidth,
-//                modifier = Modifier
-////                    .fillMaxWidth()
-////                    .height(150.dp)
-//                    .clickable { /* Handling for å vise bilde */ }
             )
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
                 Text(
                     text = photo.title,
-                    //modifier = Modifier.padding(8.dp)
                 )
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.End
                 ) {
                     ShowPhotoButton(onClick = { onShowButtonClicked(photo) })
-//                    DeletePhotoButton(onClick = { /* Slett bilde */ })
-                    OnSaveOrDeleteButtonClicked(onClick = { onSaveOrDeleteButtonClicked(photo) })
+                    if (delete) {
+                        DeletePhotoButton(onClick = { onSaveOrDeleteButtonClicked(photo)})
+                    } else {
+                        SavePhotoButton(onClick = { onSaveOrDeleteButtonClicked(photo)})
+                    }
+
                 }
             }
         }
@@ -208,19 +225,6 @@ fun ShowPhotoButton(
 }
 
 @Composable
-fun OnSaveOrDeleteButtonClicked(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Button(
-        onClick = onClick,
-        modifier = modifier
-    ) {
-        Text(stringResource(R.string.save))
-    }
-}
-
-@Composable
 fun DeletePhotoButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
@@ -230,6 +234,19 @@ fun DeletePhotoButton(
         modifier = modifier
     ) {
         Text(stringResource(R.string.delete))
+    }
+}
+
+@Composable
+fun SavePhotoButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Text(stringResource(R.string.save))
     }
 }
 @Preview(showBackground = true)
@@ -253,7 +270,8 @@ fun ErrorScreenPreview() {
 fun PhotosGridScreenPreview() {
     AlbumAppTheme {
         val mockData = List(15) { Photo(it, it,"title_test","url", "imgSrc") }
-        PhotosGridScreen(mockData, onShowButtonClicked = {}, onSaveOrDeleteButtonClicked = {})
+        PhotosGridScreen(mockData, delete = true, onShowButtonClicked = {}, onSaveOrDeleteButtonClicked = {})
+        PhotosGridScreen(mockData, delete = false, onShowButtonClicked = {}, onSaveOrDeleteButtonClicked = {})
     }
 }
 
