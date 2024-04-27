38e77c067c751a8db5d6a995530b653bb1682d4c
Therese IV <theresebvik@gmail.com>
2024-04-27 18:11:02 +0200

    viewmodel update. Started "save" function, not complete. Merged and optimized some imports.

--- app/src/main/java/com/example/albumapp/ui/screens/AlbumScreen.kt
+++ app/src/main/java/com/example/albumapp/ui/screens/AlbumScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -23,10 +22,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,15 +33,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.albumapp.R
 import com.example.albumapp.model.Photo
 import com.example.albumapp.ui.theme.AlbumAppTheme
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 
 @Composable
 fun AlbumScreen(
@@ -57,7 +49,6 @@ fun AlbumScreen(
     retryAction: () -> Unit,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
-    //viewModel: AlbumViewModel
     viewModel : AlbumViewModel = viewModel(factory = AlbumViewModel.Factory)
 ) {
     when (albumUiState) {
