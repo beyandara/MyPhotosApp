296174e675a68c71b6194b3bccb260e45122ab23
beyandara <beyan97@hotmail.com>
2024-04-27 12:57:57 +0200

    landscape mode complete

--- app/src/main/java/com/example/albumapp/ui/AlbumApp.kt
+++ app/src/main/java/com/example/albumapp/ui/AlbumApp.kt
@@ -19,9 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
@@ -103,7 +102,8 @@ fun AlbumApp(
 
 
     Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             AlbumTopAppBar(
                 currentScreen = currentScreen,
